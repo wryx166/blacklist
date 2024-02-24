47bfa90a0acba1c12d4bad180606815c0429bb38
YF-W <wryx166@outlook.com>
2024-02-25 01:48:10 +0800

    update

--- github_blacklist_generator.py
+++ github_blacklist_generator.py
@@ -1,8 +1,8 @@
 # 原始字符串  
-original_string = "github.com##.Box-sc-g0xbh4-0.bDcVHV:has-text("  
+original_string = "github.com##.Box-sc-g0xbh4-0.bDcVHV:has-text(%s)" 
   
 # 复制base_blacklist.txt的内容到blacklist.txt 
-with open("base_blacklist.txt", "r") as source_file:  
+with open("base_list.txt", "r") as source_file:  
     with open("blacklist.txt", "w") as target_file:  
         target_file.writelines(source_file)
   
@@ -16,7 +16,7 @@ with open("blacklist.txt", "a") as file:
     file.write("\n")
     for line in lines:  
         # 去除每行末尾的换行符并插入到原始字符串中  
-        new_string = original_string + line.strip() + ")"  
+        new_string = original_string % line.strip()
         file.write(new_string + "\n")  
   
 print("新内容已追加到blacklist.txt文件中！")
