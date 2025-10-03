6823fff9cfd65f8af5bd44948fa17357be10b368
wryx166 <wryx166@outlook.com>
2025-10-03 17:28:16 +0800

    update

--- bilibili/main.py
+++ bilibili/main.py
@@ -1,5 +1,13 @@
-with open("genshin", encoding="utf-8") as f:
-    genshin = f.read().replace("\n", "|")
-    with open("output.txt", "w+", encoding="utf-8") as output:
-        output.write(f"/{genshin}/\n")
-        output.write(f"//{genshin}//\n")
+import pypinyin
+
+genshinList: list[str]
+genshin: str
+with open("genshin", "r", encoding="utf-8") as f:
+    genshinList = [line for line in f.read().split("\n") if line.strip()]
+with open("genshin", "w+", encoding="utf-8") as f:
+    genshinList.sort(key=lambda x: ''.join(pypinyin.lazy_pinyin(x)))
+    f.write('\n'.join(genshinList))
+with open("output.txt", "w+", encoding="utf-8") as output:
+    genshin = '|'.join(genshinList)
+    output.write(f"/{genshin}/\n")
+    output.write(f"//{genshin}//\n")
